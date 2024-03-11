7e1296a252de33caab5d4c34de79d5da6b1baced
kabuzo13 <beahonigmann@gmail.com>
2024-03-11 19:26:52 +0000

    debugged cards appending too many times

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,13 @@
 var APIkey = "c26e48ba11f2018a306fa84c728f4266";
-// var APIkeyTwo = "f74b449b63b0653d8cb521931484c9a8";
 var city = $('#search-input');
 var searches = JSON.parse(localStorage.getItem('city')) || [];
 
 
 function getWeather(cityName) {
     console.log(cityName);
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
     var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
     var date = dayjs().format('DD/MM/YYYY');
     console.log(date);
-    // console.log(queryURL);
     console.log(fiveDay);
 
 
@@ -20,24 +17,27 @@ function getWeather(cityName) {
         })
         .then(function (data) {
             saveSearch(data.city.name);
-        
-
-            $('#today').html("<h1>" + data.city.name + " (" + date + ") " + "</h1>");
+            console.log(data);
             var icon = $('<img>').attr('src', "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png");
+            var description = data.list[0].weather[0].description;
             var tempC = data.list[0].main.temp - 273.15;
             var temp = $('<h2>').text("Temp: " + Math.round(tempC) + "C");
             var humidity = $('<h2>').text("Humidity: " + data.list[0].main.humidity + "%");
             var wind = $('<h2>').text("Wind: " + data.list[0].wind.speed + "KPH");
-            $('#today').append(icon, temp, humidity, wind);
+            var todayCard = $('<div>').addClass("card col").append("<h1>" + data.city.name + " (" + date + ") " + "</h1>",icon, description, temp, humidity, wind)
+            $('#today').empty().append(todayCard);
+
+            $('#forecast').empty();
 
             for (var i = 7; i < data.list.length; i += 8) {
                 var forecastDate = new Date(data.list[i].dt_txt).toLocaleDateString();
-                var icon = $('<img>').attr('src', "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png");
+                var icon = $('<img>').attr('src', "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png").addClass("icon");
+                var description = data.list[i].weather[0].description;
                 var tempC = data.list[i].main.temp - 273.15;
                 var temp = $('<h2>').text("Temp: " + Math.round(tempC) + "C");
                 var humidity = $('<h2>').text("Humidity: " + data.list[i].main.humidity + "%");
                 var wind = $('<h2>').text("Wind: " + data.list[i].wind.speed + "KPH");
-                var card = $('<div>').addClass("card col").append("<h1>" + data.city.name + " (" + forecastDate + ") " + "</h1>", icon, temp, humidity, wind);
+                var card = $('<div>').addClass("card col").append("<h2>" + data.city.name + "</h2>", forecastDate, icon, description, temp, humidity, wind);
                 $('#forecast').append(card);
             }
         })
